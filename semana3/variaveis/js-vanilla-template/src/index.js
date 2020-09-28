8fbf74575d1f941b4f43c4fe8047b15d4a80ea2b
Felipe Polato <fepolato@hotmail.com>
2020-09-28 14:12:20 -0300

    respostas da atividade de JS em arquivo .js

---
+++ semana3/variaveis/js-vanilla-template/src/index.js
@@ -0,0 +1,69 @@
+// 1. a = 10 
+//    b = 10
+//    console.log(b);
+//    IMPRESSO = 10
+
+//    b = 5 
+//    console.log(a, b);
+//    IMPRESSO = 10, 5
+
+// 2.  a = 10
+//     b = 20
+//     c = a
+//     b = c
+//     a = b
+//     console.log(a, b, c)
+//     IMPRESSO 10, 10, 10
+
+// let nome 
+// console.log(typeof nome);
+// let idade  
+// console.log(typeof idade);
+// undefined é a ausência do valor atribuido a variavel. 
+
+
+// let nome = prompt('Qual seu nome?');
+// console.log(nome);
+// let idade = prompt("qual sua idade?");
+// console.log( idade);
+// O retorno foi uma "string" para o nome e um number para a idade.
+// console.log(`Olá ${nome}, você tem ${idade} anos`);
+
+// let n1 = prompt("quanto tempo voce pisca os olhos?");
+// let n2 =prompt("vc acha que ganha pouco ou muito?");
+// let n3 = prompt("vc vai para uma casa de dança para?");
+// let n4 = prompt("qual diminutivo de benzão?");
+// let n5= prompt("sala só com reboco é uma?");
+// console.log(`${n1} quanto ouver ${n2} ${n3} com meu ${n4} numa  ${n5} `);
+
+
+// let comidasFavoritas = ["churrasco", "bife", "lasanha", "nhoque", "pure"];
+// console.log(`Essas são minhas comidas preferidas:
+
+// ${comidasFavoritas[0]} 
+// ${comidasFavoritas[1]} 
+// ${comidasFavoritas[2]}
+// ${comidasFavoritas[3]}
+// ${comidasFavoritas[4]}` );
+
+
+// comidasFavoritas[1] = prompt("Qual sua comida preferida");
+
+// console.log(`Essas são minhas comidas preferidas:
+
+// ${comidasFavoritas[0]} 
+// ${comidasFavoritas[1]} 
+// ${comidasFavoritas[2]}
+// ${comidasFavoritas[3]}
+// ${comidasFavoritas[4]}`);
+
+
+let quiz = ["você esta namorando?", "Voce tem dinheiro no bolso?", "Você tem olhos verdes?"];
+
+let respostas = [false, true, false];
+
+console.log(`${quiz[0]} ${respostas[0]} 
+${quiz[1]} ${respostas[1]} 
+${quiz[2]} ${respostas[2]}`);
+
+
