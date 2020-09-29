42a368ef005efeec4caa9e92afbc7874d1f513ae
Felipe Polato <fepolato@hotmail.com>
2020-09-29 14:52:49 -0300

    Exercicio atividade 29/09 utilizando js,operadore

---
+++ semana3/operadores/js-vanilla-template/src/index.js
@@ -0,0 +1,145 @@
+//1. a. false
+
+// b. false
+
+// c. true
+
+// e. boolean
+
+// 2.a. undefined ( variavel vazia)
+
+// b. null ( valor atribuido a variavel)
+
+// c. 11 (imprimiu o tamanho da array)
+
+// d. 3 (indice 1 da array)
+
+// e. [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13] (acrescentou um valor a partir do primeiro indice)
+
+// f. 9 (imprimiu o valor do indice 6)
+
+// EXERCICIO 1
+
+// let idade = prompt("Qual sua idade?");
+// let idadeAmigo = prompt("Qual a idade do seu melhor amigo(a)?");
+// let resposta1 = prompt("sua idade é maior que do seu amigo?");
+
+// let resposta2 = true ;
+// console.log(resposta2);
+// let diferenca = Number(idade) - Number(idadeAmigo);
+// console.log(diferenca);
+
+// EXERCICIO 2
+
+// let par = prompt("Digite um número par!");
+// const resultado = Number(par) % 2;
+// console.log(resultado);
+// c. Sempre que o numero for par o resto da divisão por 2 sera 0
+
+// d. caso o usuaria digite qualquer numero impar o resto da divisão será 1
+
+// EXERCICIO 3
+
+// let tarefa1 = prompt("Site uma tarefa que precise realizar!");
+// let tarefa2 = prompt("Site outra tarefa que precise realizar!");
+// let tarefa3 = prompt("Site outra tarefa que precise realizar!");
+// let listaDeTarefas = [];
+// listaDeTarefas.push(tarefa1, tarefa2, tarefa3);
+// // console.log(listaDeTarefas);
+
+// let indiceTarefas = prompt("Digite o indice de uma tarefa 0, 1 ou 2!");
+// listaDeTarefas.splice(indiceTarefas, 1);
+// console.log(listaDeTarefas);
+
+// Exercicio 4
+
+// let nome = prompt("qual seu nome?");
+// let email = prompt("Digite seu e-mail");
+// const resultado = (`O ${email} foi cadastrado com sucesso.
+// Seja bem vindo ${nome}!`);
+// console.log(resultado);
+
+// DESAFIO
+
+// 1. a.
+
+// const k1 = (- 32)*5/9 + 273.15;
+// const f1 = 77;
+// const resultado =  (f1 - 32)*5/9 + 273.15;
+// console.log(resultado);
+
+// // b.
+
+// const f2 = (0)*9/5 + 32;
+// const c2 = 80;
+// const resultado2 = (c2)*9/5 + 32;
+// console.log(resultado2);
+
+// c.
+
+// let f3 = (0)*9/5 + 32;
+// let c3 = 30;
+// let resultado3 = (c3)*9/5 + 32;
+// console.log(resultado3 + "F");
+
+// let k3 = ( - 32)*5/9 + 273.15;
+// let resultado4 = (c3 - 32)*5/9 + 273.15;
+// console.log(resultado4 + "k");
+
+// // d.
+// let valor = prompt("Valor em Celsius que deseja converter");
+// valor = Number(valor);
+// f3 = (valor)*9/5 + 32;
+// console.log(f3 + "F");
+
+// k3 = (valor - 32)*5/9 + 273.15;
+// console.log(k3 + "K");
+
+//DESAFIO 2
+
+// let consumo = 280
+// let custo = 0.05
+// let valorCobrado = custo * consumo;
+// console.log(valorCobrado);
+// let desconto = valorCobrado - (15 / 100);
+// console.log(desconto);
+
+//DESAFIO 3
+// a.
+// let lb = 20;
+// let formula = lb / 2.205;
+// console.log(`20lb equivalem a ${formula}Kg`);
+
+//b.
+// let oz = 10.5;
+// let formula2 = oz / 34;
+// console.log(`10.5oz equivalem a ${formula2}Kg`)
+
+//c.
+
+// let mi = 100;
+// let formula3 = mi * 1609;
+// console.log(`100mi equivalem a ${formula3}m`);
+
+//d.
+
+// let ft = 50;
+// let formula4 = ft / 3.281;
+// console.log(`50ft equivalem a ${formula4}m`);
+
+// e.
+// let gal = 103.56;
+// let formula5 = gal * 3.785;
+// console.log(`103.56gal equivalem a ${formula5}L`);
+
+//f.
+// let xic = 450;
+// let formula6 = xic / 3.52;
+// console.log(`450xic equivalem a ${formula6}L`);
+
+//g.
+
+// xic = prompt("Digite quatas xicaras desejar!");
+// xic = Number(xic);
+// formula6 = xic / 3.52;
+// console.log(` ${xic} xicaras Equivalem a ${formula6}xic`);
