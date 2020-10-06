e4172bd74918db316fd80107b7c366382611b4ca
Felipe Polato <fepolato@hotmail.com>
2020-10-06 17:14:53 -0300

    exercicio 06-10 evento click riscatarefa de forma dinamica

--- semana4/dom/planner/index.js
+++ semana4/dom/planner/index.js
@@ -1,12 +1,21 @@
+let contador = 0;
+
 function criarTarefa() {
   const inputTarefa = document.getElementById("tarefa");
   const diasSemanas = document.getElementById("dias-semana").value;
   const acrescentar = document.getElementById(diasSemanas);
 
+  contador = contador++;
+  let id = `tarefa-${contador}`
+  
   if (inputTarefa.value === "") {
     alert("Digite uma tarefa!!!!!");
   } else {
-    acrescentar.innerHTML += `<li>${inputTarefa.value}</li>`;
+    acrescentar.innerHTML += `<li id=${id} onclick="riscaTarefa(id)" >${inputTarefa.value}</li>`;
     inputTarefa.value = "";
   }
+}
+
+function riscaTarefa(id) {
+  document.getElementById(id).style.textDecoration = "line-through";
 }
