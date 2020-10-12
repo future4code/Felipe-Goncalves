0151d66b97fb4a18546a4981faebd79d26df524c
Felipe Polato <fepolato@hotmail.com>
2020-10-12 14:07:38 -0300

    exercicio 09/10 exercicio de funcao de array 3a

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -274,13 +274,33 @@
 
 // -------EXERCICIO 2c
 
-const array = [1, 2, 3, 4, 5, 6];
+// const array = [1, 2, 3, 4, 5, 6];
 
-const imparPar = array.map((n) => {
+// const imparPar = array.map((n) => {
+//   if (n % 2 === 1) {
+//     return `${n} é impar`;
+//   }
+//   return `${n} é par`;
+// });
+// console.log(imparPar);
 
-    if(n % 2 === 1){
-        return `${n} é impar`
-    }
-    return `${n} é par`
+// ---------EXERCICIO 3a
+
+const pessoas = [
+  { nome: "Paula", idade: 12, altura: 1.8 },
+  { nome: "João", idade: 20, altura: 1.3 },
+  { nome: "Pedro", idade: 15, altura: 1.9 },
+  { nome: "Luciano", idade: 22, altura: 1.8 },
+  { nome: "Artur", idade: 10, altura: 1.2 },
+  { nome: "Soter", idade: 70, altura: 1.9 },
+];
+
+const pessoasPermitido = pessoas.filter((a) => {
+  if (a.idade > 14 && a.idade < 60 && a.altura >= 1.5) {
+    return true;
+  }
+
+  return false;
 });
- console.log(imparPar)
+
+
