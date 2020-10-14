9d2b50726c25038d4d8de9916d38639480cf8756
Felipe Polato <fepolato@hotmail.com>
2020-10-14 14:00:30 -0300

    exercicio 14-10 componentes funcionais

---
+++ semana5/labedin/src/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./App.css";
+import CardGrande from "./components/CardGrande/CardGrande";
+import ImagemButton from "./components/ImagemButton/ImagemButton";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page-section-container">
+        <h2>Dados pessoais</h2>
+        <CardGrande
+          imagem="https://uploads-ssl.webflow.com/5d640f4558306be99cf47a0e/5dd57846babb597b77c6bb1d_PerfilFuture4_cor.png"
+          nome="Felipe Polato"
+          descricao="Me chamo Felipe Polato Gonçalves, cursei Ciencias Biologicas pela UTFPR. Conheci o mundo programação aos 30 anos e dentro de 3 meses estudando por conta propria resolvi me profissionalizar na area, hoje cursando web fullstack pela escola Labenu."
+        />
+
+        <ImagemButton
+          imagem="https://image.flaticon.com/icons/png/512/117/117472.png"
+          texto="Ver mais"
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Experiências profissionais</h2>
+        <CardGrande
+          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png"
+          nome="Labenu"
+          descricao="Em formação!"
+        />
+
+        <CardGrande
+          imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg"
+          nome="Universidade"
+          descricao="Cursado 50% do curso"
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Minhas redes sociais</h2>
+        <ImagemButton
+          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png"
+          texto="Facebook"
+        />
+
+        <ImagemButton
+          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png"
+          texto="Twitter"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
