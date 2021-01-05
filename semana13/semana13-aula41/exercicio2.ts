0306cefcfce7fa6b6887832c173601b3f4083f29
Felipe Polato <fepolato@hotmail.com>
2021-01-05 17:55:45 -0300

    aula2-semana13 typescript

---
+++ semana13/semana13-aula41/exercicio2.ts
@@ -0,0 +1,22 @@
+function obterEstatisticas(numeros: Number): number {
+    
+    const numerosOrdenados = numeros.sort(
+        (a, b) => a - b
+    )
+    
+    let soma = 0
+    
+    for (let num of numeros) {
+        soma += num
+    }
+    
+    const estatisticas = {
+        maior: numerosOrdenados[numeros.length - 1],
+        menor: numerosOrdenados[0],
+        media: soma / numeros.length
+    }
+
+    return estatisticas
+}
+
+// A- A entrada é numeros e a saida é o return.
