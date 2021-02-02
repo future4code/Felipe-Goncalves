98547ef6116ab5df9e2b37286b81d16189b0dbfd
Felipe Polato <fepolato@hotmail.com>
2021-02-02 17:32:11 -0300

    arquitetura software 2

--- semana19/semana19-aula2/src/controller/routes/teskRouter.ts
+++ semana19/semana19-aula2/src/controller/routes/teskRouter.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { createTask, getTaskById } from "../taskController";
+
+
 export const taskRouter = express.Router();
 
 taskRouter.put("/", createTask);
--- semana19/semana19-aula2/src/index.ts
+++ semana19/semana19-aula2/src/index.ts
@@ -1,17 +1,15 @@
 import express from 'express'
 import cors from 'cors'
-import { signup, login } from './controller/userController'
-import { createTask,getTaskById } from './controller/taskController'
+import { userRouter } from './controller/routes/userRouter'
+import { taskRouter } from './controller/routes/teskRouter'
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.post('/user/signup', signup)
-app.post('/user/login', login)
+app.use('/user', userRouter)
+app.use('/user', taskRouter)
 
-app.put('/task', createTask)
-app.get('/task/:id', getTaskById)
 
 app.listen(3003, () => {
    console.log('Servidor rodando na porta 3003')
