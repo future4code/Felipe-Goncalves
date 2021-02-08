cc1ef2a06b5f87371d91d82f5b15d2631a1e2b2f
Felipe Polato <fepolato@hotmail.com>
2021-02-08 18:02:34 -0300

    semana20-aula1 tests

---
+++ semana20/semana20-aula1/testes-ts-template/src/index.ts
@@ -0,0 +1,103 @@
+import knex from 'knex'
+import {config} from 'dotenv'
+
+config()
+
+export const isEven = (integer: number) => { }
+
+export const connection = knex({
+   client: "mysql",
+   connection: {
+      host: process.env.DB_HOST,
+      port: 3306,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+   },
+});
+
+export enum LOCATION {
+   EUA = "EUA",
+   BRAZIL = "BRAZIL",
+ }
+ 
+ export enum NACIONALITY {
+   BRAZILIAN = "BRAZILIAN",
+   AMERICAN = "AMERICAN",
+ }
+ 
+ export interface User {
+   name: string;
+   age: number;
+   nacionality: NACIONALITY;
+   balance: number;  
+ }
+ 
+ export interface Casino {
+   name: string;
+   location: LOCATION;
+ }
+
+ export interface Result {
+   brazilians: ResultItem;
+   americans: ResultItem;
+ }
+ 
+ export interface ResultItem {
+   allowed: string[];
+   unallowed: string[];
+ }
+
+
+export function performPurchase(user: User, value: number): User | undefined {
+	if(user.balance >= value) {
+		return {
+			...user,
+			balance: user.balance - value		
+		}
+	}
+	return undefined
+}
+
+
+export function verifyAge(casino: Casino, users: User[]): Result {
+   const allowed: User[] = [];
+   const unallowed: User[] = [];
+ 
+   for (const user of users) {
+     if (casino.location === LOCATION.EUA) {
+       if (user.age >= 21) {
+         allowed.push(user);
+       } else {
+         unallowed.push(user);
+       }
+     } else if (casino.location === LOCATION.BRAZIL) {
+       if (user.age >= 18) {
+         allowed.push(user);
+       } else {
+         unallowed.push(user);
+       }
+       break;
+     }
+   }
+ 
+   return {
+     brazilians: {
+       allowed: allowed
+         .filter((user) => user.nacionality === NACIONALITY.BRAZILIAN)
+         .map((u) => u.name),
+       unallowed: unallowed
+         .filter((user) => user.nacionality === NACIONALITY.BRAZILIAN)
+         .map((u) => u.name),
+     },
+     americans: {
+       allowed: allowed
+         .filter((user) => user.nacionality === NACIONALITY.AMERICAN)
+         .map((u) => u.name),
+       unallowed: unallowed
+         .filter((user) => user.nacionality === NACIONALITY.AMERICAN)
+         .map((u) => u.name),
+     },
+   };
+ }
+
---
+++ semana20/semana20-aula1/testes-ts-template/tests/index.test.ts
@@ -0,0 +1,143 @@
+import { performPurchase, User, verifyAge } from "../src"
+
+describe("Testing performPurchase", () => {
+
+    test("Testing balance greater than value", () => {
+        const user: User = {
+            name: "Astrodev",
+            balance: 100
+        }
+    
+        const result = performPurchase(user, 50)
+        
+        expect(result).toEqual({
+            ...user,
+            balance: 50
+        })
+    })
+    
+    test("Testing balance greater than value", () => {
+        const user: User = {
+            name: "Astrodev",
+            balance: 50
+        }
+    
+        const result = performPurchase(user, 50)
+        
+        expect(result).toEqual({
+            ...user,
+            balance: 0
+        })
+    })
+    
+    test("Testing balance greater than value", () => {
+        const user: User = {
+            name: "Astrodev",
+            balance: 100
+        }
+    
+        const result = performPurchase(user, 50)
+        
+        expect(result).toEqual({
+            ...user,
+            balance: 50
+        })
+    })
+
+})
+
+
+describe("Testing performPurchase", () => {
+
+    test("1 brazilian allowed", () => {
+        const brazilian: User = {
+          name: "Astrodev",
+          age: 19,
+          nacionality: NACIONALITY.BRAZILIAN,
+        };
+    
+        const casino: Casino = {
+          name: "Balada Estelar",
+          location: LOCATION.BRAZIL,
+        };
+    
+        const result = verifyAge(casino, [brazilian]);
+        expect(result.brazilians.allowed).toEqual(["Astrodev"]);
+      });
+
+      test("1 american allowed", () => {
+        const brazilian: User = {
+          name: "Astrodev",
+          age: 19,
+          nacionality: NACIONALITY.AMERICAN,
+        };
+    
+        const casino: Casino = {
+          name: "Balada Estelar",
+          location: LOCATION.BRAZIL,
+        };
+    
+        const result = verifyAge(casino, [brazilian]);
+        expect(result.americans.allowed).toEqual(["Astrodev"]);
+      });
+
+      test("No one allowed", () => {
+        const brazilian: User = {
+          name: "Astrodev BR",
+          age: 19,
+          nacionality: NACIONALITY.BRAZILIAN,
+        };
+    
+        const american: User = {
+          name: "Astrodev EUA",
+          age: 19,
+          nacionality: NACIONALITY.AMERICAN,
+        };
+    
+        const casino: Casino = {
+          name: "Balada Estelar",
+          location: LOCATION.EUA,
+        };
+    
+        const result = verifyAge(casino, [
+          brazilian,
+          brazilian,
+          american,
+          american,
+        ]);
+        expect(result.brazilians.unallowed).toEqual(["Astrodev BR", "Astrodev BR"]);
+        expect(result.americans.unallowed).toEqual([
+          "Astrodev EUA",
+          "Astrodev EUA",
+        ]);
+      });
+
+      test("2 american allowed and 2 brazilians unallowed", () => {
+        const brazilian: User = {
+          name: "Astrodev BR",
+          age: 19,
+          nacionality: NACIONALITY.BRAZILIAN,
+        };
+    
+        const american: User = {
+          name: "Astrodev EUA",
+          age: 21,
+          nacionality: NACIONALITY.AMERICAN,
+        };
+    
+        const casino: Casino = {
+          name: "Balada Estelar",
+          location: LOCATION.EUA,
+        };
+    
+        const result = verifyAge(casino, [
+          brazilian,
+          brazilian,
+          american,
+          american,
+        ]);
+        expect(result.brazilians.unallowed).toEqual(["Astrodev BR", "Astrodev BR"]);
+        expect(result.americans.allowed).toEqual(["Astrodev EUA", "Astrodev EUA"]);
+      });
+
+})
