5e8ffd4ee9d64e9fba2b610247b3568d21f9106a
Felipe Polato <fepolato@hotmail.com>
2021-02-10 19:05:19 -0300

    semana20-aula3 testes no back

---
+++ semana20/semana20-aula3/aula60-template/src/services/idGenerator.ts
@@ -0,0 +1,12 @@
+import { v4 } from "uuid";
+
+export class IdGenerator {
+  static generate() {
+     throw new Error("Method not implemented.");
+  }
+  public generate(): string {
+    return v4();
+  }
+}
+
+export default new IdGenerator()
